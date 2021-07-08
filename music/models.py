b03e4b514e35aea3b85f6778281fe3e54802ca34
diptangsu <diptangsu.97@gmail.com>
2021-07-08 21:51:04 +0530

    added models

--- music/models.py
+++ music/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Band(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    band = models.ForeignKey(Band, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=255)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, null=True)
+    favourite = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.name} [{self.album.band.name} ({self.album.name})'
+
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.name} ({self.get_all_songs().count()} songs)'
+
+    def get_all_songs(self):
+        return PlaylistSongs.objects.filter(playlist=self)
+
+
+class PlaylistSongs(models.Model):
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE, null=True)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE, null=True)
